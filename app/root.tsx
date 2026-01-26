6e1966e88fa9105b70230ce827f0a2848699561f
Jose Alberto Galera Sánchez <jagalerasanchez@gmail.com>
2026-01-26 17:20:28 +0100

    feat: add analytics

--- app/root.tsx
+++ app/root.tsx
@@ -53,6 +53,20 @@ export default function App() {
         <link rel="canonical" href={url} />
         <meta property="og:url" content={url} />
         <meta property="og:locale" content="es" />
+        
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X58CRXTLJ7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X58CRXTLJ7');
+            `,
+          }}
+        />
+        
         <Meta />
         <Links />
       </head>
