98d78a3b96b5765f78f0269df0c0fc8dd1003ddb
Jose Alberto Galera Sánchez <jagalerasanchez@gmail.com>
2026-01-27 11:58:09 +0100

    fix: error

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,6 @@ import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 //@ts-expect-error idk
 import stylesheet from "~/tailwind.css?url";
-import { useEffect } from "react";
 import { ThemeProvider } from "~/contexts/ThemeContext";
 import { LanguageProvider } from "~/contexts/LanguageContext";
 
@@ -28,59 +27,8 @@ export const links: LinksFunction = () => [
 export default function App() {
   const { url } = useLoaderData<typeof loader>();
 
-  useEffect(() => {
-    // Google Tag Manager
-    (function (w: any, d: Document, s: string, l: string, i: string) {
-      w[l] = w[l] || [];
-      w[l].push({
-        "gtm.start": new Date().getTime(),
-        event: "gtm.js",
-      });
-      const f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s) as HTMLScriptElement,
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode?.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-WP44KWBC");
-
-    // Google Analytics 4
-    const gaScript = document.createElement('script');
-    gaScript.async = true;
-    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-X58CRXTLJ7';
-    document.head.appendChild(gaScript);
-
-    (window as any).dataLayer = (window as any).dataLayer || [];
-    function gtag(...args: any[]) {
-      (window as any).dataLayer.push(args);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-X58CRXTLJ7');
-
-    // Meta Pixel
-    (function(f: any, b: Document, e: string, v: string) {
-      if (f.fbq) return;
-      const n: any = f.fbq = function() {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = true;
-      n.version = '2.0';
-      n.queue = [];
-      const t = b.createElement(e) as HTMLScriptElement;
-      t.async = true;
-      t.src = v;
-      const s = b.getElementsByTagName(e)[0];
-      s.parentNode?.insertBefore(t, s);
-    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-    (window as any).fbq('init', '909301398094959');
-    (window as any).fbq('track', 'PageView');
-  }, []);
-
   return (
-    <html lang="es" className="dark" suppressHydrationWarning>
+    <html lang="es" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -89,10 +37,46 @@ export default function App() {
         <meta property="og:url" content={url} />
         <meta property="og:locale" content="es" />
         
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X58CRXTLJ7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X58CRXTLJ7');
+            `,
+          }}
+        />
+        
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '909301398094959');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=909301398094959&ev=PageView&noscript=1"
+          />
+        </noscript>
+        
         <Meta />
         <Links />
       </head>
-      <body className="bg-[#030303] dark:bg-[#030303] light:bg-white text-gray-300 dark:text-gray-300 light:text-gray-700 transition-colors duration-300" suppressHydrationWarning>
+      <body className="bg-[#030303] dark:bg-[#030303] light:bg-white text-gray-300 dark:text-gray-300 light:text-gray-700 transition-colors duration-300">
         <noscript>
           <iframe
             title="#"
